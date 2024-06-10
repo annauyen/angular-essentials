60e6ade16f917494ce7d1eed312d0f5c0d85cb4e
bnnuyen <nhiuyenb@gmail.com>
2024-06-10 15:17:05 +0700

    add data binding feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { NgFor } from '@angular/common';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, ServerElementComponent, NgFor, CockpitComponent],
 })
 export class AppComponent {
-  title = 'server-element-app';
+  serverElements = [
+    { type: 'server', name: 'ohohoho', content: 'test' },
+    { type: 'blueprint', name: 'ohohoho', content: 'test' },
+  ];
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+  onBlueprintAdded(blueprintData: {
+    blueprintName: string;
+    blueprintContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent,
+    });
+  }
 }
